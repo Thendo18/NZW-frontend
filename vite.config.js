7c7ef94a613257773d7226ac1a7fea17974ca80d
Thendo Makherana <thendom@touchsides.com>
2025-05-26 23:28:18 +0200

    fixed vite for deployment purposes

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "NZWalks.UI";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-    const target = 'https://localhost:7135';
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
     resolve: {
@@ -43,15 +12,16 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target,
+                target: 'https://localhost:7135',
                 changeOrigin: true,
                 secure: false,
             }
         },
-        port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
-    }    
-})
+        port: 5173
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+        minify: 'terser'
+    }
+});
